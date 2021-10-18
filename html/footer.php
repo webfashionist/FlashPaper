8b7825a41d97358ec045b2632bf1b2936ca49df0
gubb_it <info@webfashion.in>
2021-10-18 18:39:29 +0200

    Fix footer for mobile devices

--- html/footer.php
+++ html/footer.php
@@ -1,5 +1,5 @@
 <?php defined('_DIRECT_ACCESS_CHECK') or exit(); ?>
-		<footer style="width: 100%; height: 30px; line-height: 30px; background-color: #f5f5f5;" class="fixed-bottom">
+		<footer style="width: 100%; line-height: 30px; background-color: #f5f5f5;" class="fixed-bottom">
 			<div class="container">
 				<span class="text-muted small">Source code for FlashPaper is freely available at <a href="https://github.com/webfashionist/FlashPaper">Github.com</a>, forked from <a href="https://github.com/AndrewPaglusch/FlashPaper">AndrewPaglusch/FlashPaper</a></span>
 			</div>
